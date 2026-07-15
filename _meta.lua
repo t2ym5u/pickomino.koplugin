1a222b5d62f64e909c245de30f20e82728b5c4d7
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-15 14:20:58 +0200

    chore: bump version to 1.1.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name        = "pickomino",
     fullname    = _("Pickomino"),
     description = _("Combinatorial dice and tile logic game"),
-    version     = "1.1.0",
+    version     = "1.1.1",
 }
